0138d32f238f0f8a1183258ddad889ed520c5501
Mauricio Abreu <mauriciogamer163@gmail.com>
2024-09-04 17:19:22 -0400

    código js

---
+++ script.js
@@ -0,0 +1,10 @@
+const n1 = 20.05, n2 = 50, n3 = 20, n4 = 10.11
+let soma, subtracao, multiplicacao, divisao
+
+soma = n1+n2+n3+n4
+subtracao = n1-n2-n3-n4
+multiplicacao = n1*n2*n3*n4
+divisao = (n1+n2+n3+n4) / 4
+
+console.log(soma + "\n" + subtracao + "\n" + multiplicacao + "\n" + divisao);
+
